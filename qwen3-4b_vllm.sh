390479fafdf23b83fe4bc3a167b8fc68909a87e9
Xingjian <xingjian.zhang@qq.com>
2025-12-05 21:57:07 +0800

    Add 4B bs len bench

---
+++ qwen3-4b_vllm.sh
@@ -0,0 +1,14 @@
+MODEL=/nfs/xjzhang/Qwen/Qwen3-4B
+MODEL_NAME=${MODEL##*/}
+DATE=`date +%Y%m%d_%H%M%S`
+
+python3 profile_vllm_cli.py \
+    --model $MODEL \
+    --batched-tokens 100000 \
+    --batch-sizes 1 2 4 8 16 32 64 128 \
+    --prompt-length 512 1024 2048 4096 8192 16384 \
+    --output-len 128 \
+    --window-size 200 \
+    --num-repeat 1 \
+    --log-dir vllm_step_${MODEL_NAME}_${DATE} \
+    --gpu-memory-utilization 0.95
