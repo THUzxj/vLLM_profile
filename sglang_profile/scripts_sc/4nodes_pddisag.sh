81ac8c4cf88fd00b01c1ff079a238f819c6f4f53
Xingjian <xingjian.zhang@qq.com>
2026-03-28 00:10:37 +0800

    update 2nodes and 4nodes pddisag scripts

--- sglang_profile/scripts_sc/4nodes_pddisag.sh
+++ sglang_profile/scripts_sc/4nodes_pddisag.sh
@@ -1,12 +1,11 @@
 
-NFS_SHARED_DIR="/path_to_ips" \
 MAX_RUNNING_REQUESTS_DECODE=128 \
 BASE_PORT=8000 \
-PREFILL_NODES=1 \
-DECODE_NODES=1 \
+PREFILL_NODES=2 \
+DECODE_NODES=2 \
 ARCHITECTURE="H" \
 MEM_FRACTION_STATIC=0.7 \
-DP=8 EP=8 TP=8 \
+DP=16 EP=16 TP=16 \
 MODEL_PATH="/path_to_model" \
 EP_NUM_REDUNDANT_EXPERTS=32 \
 MEM_CHUNKED_PREFILL_SIZE=16384 \
@@ -20,4 +19,3 @@ IL="1000" BS="256" OL="100" PROFILE_STEPS=50 \
 bash scripts_sc/launch_and_bench_server_pddisagg.sh \
 scripts_sc/launch_server_deepseek-v3_formal_torchprofile_pddisagg_H.sh \
 scripts_sc/bench_one_batch_server_profile_metrics_trigger.sh
-
